9b8a6bff23ced3549991f36ddbda4986f99afd56
Kimuntu <patrick.nii@aol.com>
2025-03-12 18:20:22 +0100

    add comment

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
-import sys
-sys.path.append('/home/kimuntu/Documents/Programming2025/Drone_AI/my-env/lib/python3.12/site-packages')
-
 from ultralytics import YOLO
 
-
+# Load the model
 model = YOLO("yolov8n.pt")
+
+# Track the video source 0, save the output video in the project folder, name the output video as test_yolo, and show the output video
 test = model.track(source=0, save=True, project="./output", name="test_yolo", show=True)
