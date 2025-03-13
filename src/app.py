d5cff408ce1f798daf7fc6e7b11afa503790fb6a
Kimuntu <patrick.nii@aol.com>
2025-03-13 18:21:50 +0100

    down grade version model

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-# Load the model yolov Extra large model with high accuracy
-model = YOLO("yolov11x.pt")
+# Load the model yolov8x.pt Extra large model with high accuracy
+model = YOLO("yolov8x.pt")
 
 # Track the video source 0, save the output video in the project folder, name the output video as test_yolo, and show the output video
 source_0_ = 0
